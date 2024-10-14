b66a2a8e1137e2134a553b49da81262f23060b82
X <qbrext@gmail.com>
2024-10-14 10:38:01 +0100

    Need to fix DatabaseSeeder

---
+++ MyProject/Context/DatabaseSeeder.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using MyProject.Model;
+
+namespace MyProject.Context
+{
+
+    public class DatabaseSeeder
+    {
+        private readonly DatabaseContext _context;
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public DatabaseSeeder(DatabaseContext context, UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _context = context;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public async Task Seed()
+        {
+            await _context.Database.MigrateAsync();
+
+            if (!_context.Users.Any())
+            {
+                await _roleManager.CreateAsync(new IdentityRole("Admin"));
+                await _roleManager.CreateAsync(new IdentityRole("Customer"));
+
+                var adminEmail = "admin@r2.com";
+                var adminPassword = "Cheese123!";
+
+                var admin = new User
+                {
+                    UserName = adminEmail,
+                    Email = adminEmail,
+                    /* FirstName = "Admin",
+                    LastName = "User",
+                    Address = "123 Admin Street",
+                    City = "Adminville",
+                    PostCode = "AD12 MIN"*/
+                };
+
+                await _userManager.CreateAsync(admin, adminPassword);
+                await _userManager.AddToRoleAsync(admin, "Admin");
+            }
+
+        }
+
+}
