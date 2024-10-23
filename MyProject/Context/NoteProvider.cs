d3c337fcf088b8b279a6a61b0d65d5ac5a5af75c
X <qbrext@gmail.com>
2024-10-23 10:30:49 +0100

    Adding removing notes

--- MyProject/Context/NoteProvider.cs
+++ MyProject/Context/NoteProvider.cs
@@ -18,9 +18,27 @@ namespace MyProject.Context
             return await _context.Notes.ToListAsync();
         }
 
+        public async Task<List<Note>> GetNoteByUserAsync(User user)
+        {
+            if (user == null)
+            {
+                return new List<Note>();
+            }
+
+            return await _context.Notes
+                .Where(note => note.User.Id == user.Id)
+                .ToListAsync();
+        }
+
         public async Task AddNoteAsync(Note note)
         {
             _context.Notes.Add(note);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteNoteAsync(Note note)
+        {
+            _context.Notes.Remove(note);
             await _context.SaveChangesAsync();
         }
 
