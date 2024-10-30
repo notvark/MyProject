c348f6b16d56ddf12d92165c942ad3cfb29e5c7d
X <mbprofessionalworkinbox@gmail.com>
2024-10-30 00:12:46 +0000

    Fix comments stuff

--- MyProject/Context/PostCommentProvider.cs
+++ MyProject/Context/PostCommentProvider.cs
@@ -13,5 +13,41 @@ namespace MyProject.Context
             _context = context;
         }
 
+        public async Task<List<PostComment>> GetCommentByPostAsync(Post post)
+        {
+            if (post == null)
+            {
+                return new List<PostComment>();
+            }
+
+            return await _context.PostComments
+                .Where(postcomment => postcomment.Post.Id == post.Id)
+                .ToListAsync();
+        }
+
+        public async Task<List<PostComment>> GetCommentByUserAsync(User user)
+        {
+            if (user == null)
+            {
+                return new List<PostComment>();
+            }
+
+            return await _context.PostComments
+                .Where(postcomment => postcomment.User.Id == user.Id)
+                .ToListAsync();
+        }
+
+        public async Task AddPostCommentAsync(PostComment PostComment)
+        {
+            _context.PostComments.Add(PostComment);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeletePostCommentAsync(PostComment PostComment)
+        {
+            _context.PostComments.Remove(PostComment);
+            await _context.SaveChangesAsync();
+        }
+
     }
 }
